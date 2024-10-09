28464e98d6dd9cd4e273621004c389d5f83c2a2b
MungChi <2ee42a45@gmail.com>
2024-10-09 22:46:13 +0900

    Feat: 배달의 민족 서비스 구현 프로젝트 생성

---
+++ JungHyeonmin/baedal-minjok/src/main/java/com/ohgiraffers/baedalminjok/BaedalMinjokApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.baedalminjok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaedalMinjokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaedalMinjokApplication.class, args);
+	}
+
+}
